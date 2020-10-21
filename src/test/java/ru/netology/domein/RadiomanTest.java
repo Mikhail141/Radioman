745163b8e05a26352fbec79e73d4fadbf486b360
Mikhail141 <70147484+Mikhail141@users.noreply.github.com>
2020-10-21 18:53:31 +0300

    change Test

--- src/test/java/ru/netology/domein/RadiomanTest.java
+++ src/test/java/ru/netology/domein/RadiomanTest.java
@@ -5,78 +5,76 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadiomanTest {
+    Radioman radioman = new Radioman();
 
     @Test
     void getNumberstation() {
-        Radioman radioman = new Radioman();
-        int maxstation = 9;
+        int maxstation = 10;
         int minstation = 0;
-
         radioman.setCurrentNumberstation(3);
         assertEquals(3,radioman.getCurrentNumberstation());
-
     }
 
     @Test
     void getSound() {
-        Radioman radioman = new Radioman();
-
-        int maxsound = 10;
+        int maxsound = 100;
         int minsound = 0;
-
-        radioman.setCurrentSound(10);
-        assertEquals(10,radioman.getCurrentSound());
+        radioman.setCurrentSound(100);
+        assertEquals(100,radioman.getCurrentSound());
     }
 
 
     @Test
-    void increaseCurrentNumberstation() {
-        Radioman radioman = new Radioman();
-
-        radioman.setCurrentNumberstation(9);
+    void increaseCurrentNumberstationMax() {
+        radioman.setCurrentNumberstation(10);
         radioman.increaseCurrentNumberstation();
         assertEquals(0,radioman.getCurrentNumberstation());
 
-        radioman.setCurrentNumberstation(0);
-        radioman.increaseCurrentNumberstation();
-        assertEquals(1,radioman.getCurrentNumberstation());
-
     }
 
     @Test
-    void decreaseCurrentNumberstation() {
-        Radioman radioman = new Radioman();
+    void increaseCurrentNumberstation() {
+        radioman.setCurrentNumberstation(0);
+        radioman.increaseCurrentNumberstation();
+        assertEquals(1,radioman.getCurrentNumberstation());
+    }
 
+    @Test
+    void decreaseCurrentNumberstationMin() {
         radioman.setCurrentNumberstation(0);
         radioman.decreaseCurrentNumberstation();
-        assertEquals(9,radioman.getCurrentNumberstation());
+        assertEquals(10,radioman.getCurrentNumberstation());
+    }
 
+    @Test
+    void decreaseCurrentNumberstation(){
         radioman.setCurrentNumberstation(8);
         radioman.decreaseCurrentNumberstation();
         assertEquals(7,radioman.getCurrentNumberstation());
     }
 
     @Test
-    void increaseCurrentSound() {
-        Radioman radioman = new Radioman();
-
-        radioman.setCurrentSound(10);
+    void increaseCurrentSoundMax() {
+        radioman.setCurrentSound(100);
         radioman.increaseCurrentSound();
-        assertEquals(10,radioman.getCurrentSound());
+        assertEquals(100,radioman.getCurrentSound());
+    }
 
+    @Test
+    void increaseCurrentSound(){
         radioman.setCurrentSound(7);
         radioman.increaseCurrentSound();
         assertEquals(8,radioman.getCurrentSound());
     }
 
     @Test
-    void decreaseCurrentSound() {
-        Radioman radioman = new Radioman();
-
+    void decreaseCurrentSoundMin() {
         radioman.setCurrentSound(0);
         radioman.decreaseCurrentSound();
         assertEquals(0,radioman.getCurrentSound());
-
+    }
+    @Test
+    void decreaseCurrentSound(){
         radioman.setCurrentSound(8);
         radioman.decreaseCurrentSound();
         assertEquals(7,radioman.getCurrentSound());
